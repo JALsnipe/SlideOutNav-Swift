766354c6b3462dbfe6be06455925faa2fad593a7
Josh Lieberman <josh.lieberman92@gmail.com>
2015-04-01 17:33:37 -0400

    adding footer to menu for cleaner look

--- SlideOutNavigationTest/MenuTableViewController.swift
+++ SlideOutNavigationTest/MenuTableViewController.swift
@@ -26,6 +26,8 @@ class MenuTableViewController: UITableViewController {
 
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+        
+        self.tableView.tableFooterView = UIView(frame: CGRectZero)
     }
 
     override func didReceiveMemoryWarning() {
